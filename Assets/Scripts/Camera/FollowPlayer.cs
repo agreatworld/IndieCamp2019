6ab11a60d747a7df54a24804f2b78700c49e2790
whongquan <acupoffancy@gmail.com>
2019-12-13 17:47:00 +0800

    基础框架

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	/// <summary>
+	/// 摄像机
+	/// </summary>
+	private Camera camera;
+
+	private float cameraViewWidthExtends;
+
+	private float cameraViewHeightExtends;
+
+	/// <summary>
+	/// 跟随目标
+	/// </summary>
+	public GameObject target;
+
+	/// <summary>
+	///  地图边界
+	/// </summary>
+	public GameObject mapBound;
+
+	/// <summary>
+	/// 地图边界碰撞器
+	/// </summary>
+	private BoxCollider2D mapCollider;
+
+	/// <summary>
+	/// 相机移动的时长
+	/// </summary>
+	private float smoothTime = 0.1f;
+
+	private Vector3 currentVelocity = Vector3.zero;
+
+	// Start is called before the first frame update
+	void Start() {
+		camera = GetComponent<Camera>();
+		mapCollider = mapBound.GetComponent<BoxCollider2D>();
+		cameraViewHeightExtends = camera.orthographicSize;
+		cameraViewWidthExtends = cameraViewHeightExtends * camera.aspect;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		Vector3 targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+		if (targetPosition.x - cameraViewWidthExtends < mapCollider.bounds.min.x || targetPosition.x + cameraViewWidthExtends > mapCollider.bounds.max.x) {
+			targetPosition.x = transform.position.x;
+		}
+		if (targetPosition.y - cameraViewHeightExtends < mapCollider.bounds.min.y || targetPosition.y + cameraViewHeightExtends > mapCollider.bounds.max.y) {
+			targetPosition.y = transform.position.y;
+		}
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private float horizontalSpeed = 8;
+	private float verticalSpeed = 8;
+	private Vector2 velocity;
+	private List<RaycastHit2D> hits = new List<RaycastHit2D>();
+
+	// Update is called once per frame
+	void Update() {
+		MoveByGetAxis();
+		UpdateHits();
+	}
+
+	private void LateUpdate() {
+		HandleHits();
+	}
+
+	private void HandleHits() {
+		foreach (var hit in hits) {
+			if (hit.collider != null) {
+				Debug.Log(hit.transform.name);
+			}
+		}
+	}
+
+	private void MoveByGetAxis() {
+		float translationX = Input.GetAxis("Horizontal") * Time.deltaTime * horizontalSpeed;
+		float translationY = Input.GetAxis("Vertical") * Time.deltaTime * verticalSpeed;
+		Vector2 translation = new Vector2(translationX, translationY);
+		velocity = translation;
+		transform.Translate(translation);
+	}
+
+	private void UpdateHits() {
+		hits.Clear();
+		Vector2 origin = transform.position;
+		Vector2 direction = velocity;
+		float distance = 1;
+		Debug.DrawRay(origin, direction);
+		hits.Add(Physics2D.Raycast(origin, direction, distance));
+	}
+}
