486d75003ef3bfaac9d315442bd24956241132f1
whongquan <acupoffancy@gmail.com>
2019-12-13 19:37:28 +0800

    射线检测

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,14 +20,6 @@ public class PlayerController : MonoBehaviour {
 		HandleHits();
 	}
 
-	private void HandleHits() {
-		foreach (var hit in hits) {
-			if (hit.collider != null) {
-				Debug.Log(hit.transform.name);
-			}
-		}
-	}
-
 	private void MoveByGetAxis() {
 		float translationX = Input.GetAxis("Horizontal") * Time.deltaTime * horizontalSpeed;
 		float translationY = Input.GetAxis("Vertical") * Time.deltaTime * verticalSpeed;
@@ -35,12 +28,36 @@ public class PlayerController : MonoBehaviour {
 		transform.Translate(translation);
 	}
 
+	/// <summary>
+	/// 每一帧进行一次针对所有需要层级的射线检测，一帧调用一次
+	/// </summary>
 	private void UpdateHits() {
 		hits.Clear();
-		Vector2 origin = transform.position;
-		Vector2 direction = velocity;
-		float distance = 1;
-		Debug.DrawRay(origin, direction);
-		hits.Add(Physics2D.Raycast(origin, direction, distance));
+		Vector2 rayOrigin = transform.position;
+		Vector2 rayDirection = velocity;
+		float rayDistance = 1;
+		int layerMask = 0;
+		// 用 layerMask 控制检测的各层级
+		layerMask = 1 << 8;
+		CastRaysWithLayerMask(rayOrigin, rayDirection, rayDistance, layerMask);
+		
+	}
+
+
+	private void CastRaysWithLayerMask(Vector2 rayOrigin, Vector2 rayDirection, float rayDistance, int layerMask) {
+		Debug.DrawRay(rayOrigin, rayDirection.normalized * rayDistance);
+		RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance, layerMask);
+		if (hit.collider != null) {
+			hits.Add(hit);
+		}
+	}
+
+	private void HandleHits() {
+		foreach (var hit in hits) {
+			if (hit.collider != null) {
+				Debug.Log($"碰撞检测到 {hit.transform.name}");
+			}
+		}
+		hits.Clear();
 	}
 }
