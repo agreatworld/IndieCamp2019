9eef3f2399194b91069afe460d7a467f37fa805f
whongquan <acupoffancy@gmail.com>
2019-12-14 23:17:23 +0800

    开始界面和场景跳转

---
+++ Assets/Scripts/UI/ButtonAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using DG.Tweening;
+
+public class ButtonAnimation : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
+	public void OnPointerClick(PointerEventData eventData) {
+		if (transform.name == "Begin") {
+			SceneManager.LoadScene("MainScene");
+		}
+	}
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		transform.DOScale(Vector2.one * 1.2f, 0.2f);
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		transform.DOScale(Vector2.one, 0.1f);
+	}
+
+	// Start is called before the first frame update
+	void Start() {
+	}
+
+}
